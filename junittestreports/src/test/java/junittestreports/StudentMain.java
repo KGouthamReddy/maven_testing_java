1fad416126780277974abae691fc9c70eb725ebb
KGouthamReddy <kasireddygouthamreddy74135@gmail.com>
2022-07-17 12:33:06 +0530

    testing changes

---
+++ junittestreports/src/test/java/junittestreports/StudentMain.java
@@ -0,0 +1,17 @@
+//package junittestreports;
+//import java.util.Scanner;
+//
+//public class StudentMain {
+//	public static void main(String args[]) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		Student[] arr;
+//		int n;
+//		
+//		System.out.println("Enter how many students you want to create? ");
+//		n = sc.nextInt();
+//		
+//		arr = new Student[n];
+//		
+//	}
+//}
